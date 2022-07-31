ac3422d2f43b55e62aa6ff43591848bb00facbd0
Vladislava96 <vladislava96.96@mail.ru>
2022-07-31 16:01:32 +0300

    fix: descending sort

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 module.exports = function towelSort (matrix) {
-  let newArr = [];
-  matrix.map(arr => {
-    arr.map(elem => {
-      newArr.push(elem)
-    })
-  })
-  function sort(arr) {
+
+  function sortAscending(arr) {
     if (arr.length <= 1) {
       return arr
     }
@@ -25,7 +20,32 @@ module.exports = function towelSort (matrix) {
         large.push(arr[i])
       }
     }
-    return [...sort(smaller), mediumElem, ...sort(large)]
+    return [...sortAscending(smaller), mediumElem, ...sortAscending(large)]
   }
-  return sort(newArr)
+
+  function sortDescending(arr) {
+    if (arr.length <= 1) {
+      return arr
+    }
+    let smaller = [];
+    let large = [];
+    
+    let mediumIndex = Math.floor(arr.length / 2);
+    let mediumElem = arr[mediumIndex];
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (i === mediumIndex) {
+        continue
+      }
+      if (arr[i] < mediumElem) {
+        smaller.push(arr[i])
+      } else {
+        large.push(arr[i])
+      }
+    }
+    return [...sortDescending(large), mediumElem, ...sortDescending(smaller)]
+  }
+
+  let newMatrix = matrix.map((arrMin, index) => index%2 === 0 ? sortAscending(arrMin) : sortDescending(arrMin))
+  return newMatrix
 }
