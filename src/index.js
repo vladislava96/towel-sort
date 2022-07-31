9abe37735df776c2cd1f8a4aeab9aa74f600dab8
Vladislava96 <vladislava96.96@mail.ru>
2022-07-31 15:31:41 +0300

    init: start towel sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArr = [];
+  matrix.map(arr => {
+    arr.map(elem => {
+      newArr.push(elem)
+    })
+  })
+  function sort(arr) {
+    if (arr.length <= 1) {
+      return arr
+    }
+    let smaller = [];
+    let large = [];
+    
+    let mediumIndex = Math.floor(arr.length / 2);
+    let mediumElem = arr[mediumIndex];
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (i === mediumIndex) {
+        continue
+      }
+      if (arr[i] < mediumElem) {
+        smaller.push(arr[i])
+      } else {
+        large.push(arr[i])
+      }
+    }
+    return [...sort(smaller), mediumElem, ...sort(large)]
+  }
+  return sort(newArr)
 }
